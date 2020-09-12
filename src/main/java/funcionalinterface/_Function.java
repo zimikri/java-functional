b29c41e86b40513f300e228b4a4be7595ca2d0aa
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-09-13 01:16:27 +0200

    functional style

---
+++ src/main/java/funcionalinterface/_Function.java
@@ -0,0 +1,19 @@
+package funcionalinterface;
+
+import java.util.function.Function;
+
+public class _Function {
+    public static void main(String[] args) {
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
+
+    static int incrementByOne(int number) {
+        return ++number;
+    }
+}
