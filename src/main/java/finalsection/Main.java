30c24694427953912f4ae20093f18b01a83823b9
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-09-13 18:09:49 +0200

    callback function with Consumer

---
+++ src/main/java/finalsection/Main.java
@@ -0,0 +1,38 @@
+package finalsection;
+
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        hello(
+                "John",
+                null,
+                value -> System.out.println("Last name not provided for " + value)
+        );
+
+        hello2(
+                "John",
+                null,
+                () -> System.out.println("Last name not provided")
+        );
+    }
+
+    static void hello(String firstName, String lastName, Consumer<String> callback) {
+        System.out.println(firstName);
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable callback) {
+        System.out.println(firstName);
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.run();
+        }
+    }
+}
